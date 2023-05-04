4c7084deb3812eae1b8dfac1e541b04f5990dff1
Nicowski25 <nicola.faedo0610@gmail.com>
2023-05-04 12:24:00 +0200

    snack 1

---
+++ snack1/index.php
@@ -0,0 +1,25 @@
+<?php
+    $games = [
+        [
+            'home' => 'Olimpia Milano',
+            'away' => 'Cantu',
+            'home_points' => '55',
+            'away_points' => '60'
+        ]
+    ]
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+</head>
+<body>
+    <?php foreach($games as $game):?>
+        <p><?= $game['home']. '- '.$game['away'].' | '.$game['home_points'].' - '.$game['away_points']?></p>
+    <?php endforeach; ?>
+</body>
+</html>
